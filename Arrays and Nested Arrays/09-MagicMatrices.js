e47465de2af54bae3d0781375cf790d7f7fc607a
iva-garlova <139470536+iva-garlova@users.noreply.github.com>
2024-01-23 17:38:37 +0200

    Update 09-MagicMatrices.js

--- Arrays and Nested Arrays/09-MagicMatrices.js
+++ Arrays and Nested Arrays/09-MagicMatrices.js
@@ -1,22 +1,22 @@
-function solve(args) {
+function solve(matrices) {
 
-    let initialSum = args[0].reduce((a, b) => a + b);
+    let initialSum = matrices[0].reduce((a, b) => a + b);
 
-    for(let i = 0; i < args.length; i++) {
-        let rowSum = 0;
-        let colSum = 0;
+    for(let i = 0; i < matrices.length; i++) {
+        let row = 0;
+        let col = 0;
 
-        for(let j = 0; j < args.length; j++) {
-            rowSum += args[i][j];
-            colSum += args[j][i];
+        for(let j = 0; j < matrices.length; j++) {
+            row += matrices[i][j];
+            col += matrices[j][i];
         }
 
-        if (initialSum !== rowSum && initialSum !== colSum) {
-            console.log('false');
+        if (initialSum !== row && initialSum !== col) {
+            return 'false';
         }
     }
 
-    console.log('true');
+    return 'true';
 }
 //solve([[4, 5, 6], [6, 5, 4], [5, 5, 5]]);
 solve([[11, 32, 45], [21, 0, 1], [21, 1, 1]]);
