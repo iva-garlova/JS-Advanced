6a99dd7bf17f3b93e2855d86f26068ab25c4492f
iva-garlova <139470536+iva-garlova@users.noreply.github.com>
2024-02-06 18:24:05 +0200

    1

---
+++ DOM Manipulations/04. Fill-Dropdown/dropdown.js
@@ -0,0 +1,18 @@
+function addItem() {
+    const selectmenuref = document.getElementById("menu");
+    const textInputRef = document.getElementById("newItemText");
+    const keyInputRef = document.getElementById("newItemValue");
+
+    let textValue = textInputRef.value;
+    let keyValue = keyInputRef.value;
+    
+    let option = document.createElement("option");
+    option.value = keyValue;
+    option.text = textValue;
+
+    selectmenuref.appendChild(option);
+
+    textInputRef.value = "";
+    keyInputRef.value = "";
+
+}
